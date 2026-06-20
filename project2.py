30a24ced373ba41b8a96b4ab5c13913ec9324efd
Nikita <yadavs57740@gmail.com>
2026-06-20 08:35:20 +0530

    first commit

--- project2.py
+++ project2.py
@@ -2,21 +2,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 # Creating ecommerce data
 
 data = {
     "Order_ID": np.arange(101,111),
+
     "Product": [
         "Laptop","Mobile","Shoes","Watch","Laptop",
         "Headphone","Mobile","Shoes","Watch","Tablet"
     ],
+
     "Category": [
         "Electronics","Electronics","Fashion","Fashion",
         "Electronics","Electronics","Electronics",
         "Fashion","Fashion","Electronics"
     ],
+
     "Price": np.random.randint(1000,50000,10),
+
     "Quantity": np.random.randint(1,5,10),
+
     "Month": [
         "Jan","Jan","Feb","Feb","Mar",
         "Mar","Apr","Apr","May","May"
@@ -26,7 +32,9 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Revenue calculation
+
+# Revenue
+
 df["Revenue"] = df["Price"] * df["Quantity"]
 
 
@@ -35,25 +43,33 @@ print(df)
 
 
 # Total sales
+
 total_sales = np.sum(df["Revenue"])
+
 print("\nTotal Revenue:", total_sales)
 
 
+
 # Best selling product
+
 best_product = df.groupby("Product")["Quantity"].sum()
 
 print("\nBest Selling Product:")
 print(best_product.sort_values(ascending=False).head(1))
 
 
+
 # Category wise revenue
+
 category_sales = df.groupby("Category")["Revenue"].sum()
 
 print("\nCategory Revenue:")
 print(category_sales)
 
 
+
 # Monthly sales
+
 monthly_sales = df.groupby("Month")["Revenue"].sum()
 
 print("\nMonthly Sales:")
@@ -61,25 +77,32 @@ print(monthly_sales)
 
 
 
-# Graph 1 Product Revenue
+# -------- ALL CHARTS IN ONE FIGURE --------
 
-plt.figure(figsize=(8,5))
 
-plt.bar(df["Product"],df["Revenue"])
+plt.figure(figsize=(15,5))
+
+
+# Chart 1 Product Revenue
+
+plt.subplot(1,3,1)
+
+plt.bar(
+    df["Product"],
+    df["Revenue"]
+)
 
 plt.xticks(rotation=45)
 
-plt.title("Product Revenue Analysis")
+plt.title("Product Revenue")
 plt.xlabel("Products")
 plt.ylabel("Revenue")
 
-plt.show()
 
 
+# Chart 2 Category Sales
 
-# Graph 2 Category Sales
-
-plt.figure(figsize=(6,4))
+plt.subplot(1,3,2)
 
 plt.pie(
     category_sales,
@@ -87,15 +110,13 @@ plt.pie(
     autopct="%1.1f%%"
 )
 
-plt.title("Category Wise Sales")
-
-plt.show()
+plt.title("Category Sales")
 
 
 
-# Graph 3 Monthly Sales
+# Chart 3 Monthly Sales
 
-plt.figure(figsize=(7,4))
+plt.subplot(1,3,3)
 
 plt.plot(
     monthly_sales.index,
@@ -104,7 +125,18 @@ plt.plot(
 )
 
 plt.title("Monthly Revenue Trend")
+
 plt.xlabel("Month")
 plt.ylabel("Revenue")
+
+plt.grid()
+
+
+# Adjust spacing
+
+plt.tight_layout()
+
+
+# Show all
 
 plt.show()
